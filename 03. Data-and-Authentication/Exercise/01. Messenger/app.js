8c425be2d88bc2fef4756b02a559fdd91b3179a2
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-06 11:48:05 +0200

    Update app.js

--- 03. Data-and-Authentication/Exercise/01. Messenger/app.js
+++ 03. Data-and-Authentication/Exercise/01. Messenger/app.js
@@ -1,5 +1,35 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    loadMessages(); 
+    document.getElementById('refresh').addEventListener('click', loadMessages);
 }
 
 attachEvents();
+
+async function loadMessages() {
+
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const messages = Object.values(data);
+
+    const list = document.getElementById('messages');
+    list.value = messages.map(m => `${m.author}: ${m.content}`).join('\n');
+}
+
+async function createMessage(message) {
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    }
+
+    const res = await fetch(url, options);
+    const result = await res.json();
+
+    return result;
+}
