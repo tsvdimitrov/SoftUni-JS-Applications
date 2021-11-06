8f90032c3fbc23d03b553e786141f1218057987a
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-06 20:35:00 +0200

    Update app.js

--- 03. Data-and-Authentication/Exercise/04. Book-library/app.js
+++ 03. Data-and-Authentication/Exercise/04. Book-library/app.js
@@ -1,5 +1,39 @@
 const tbody = document.querySelector('tbody');
 document.getElementById('loadBooks').addEventListener('click', loadBooks);
+document.getElementById('createForm').addEventListener('submit', onCreate);
+tbody.addEventListener('click', onTableClick);
+
+loadBooks();
+
+function onTableClick(event) {
+
+    if (event.target.className == 'delete') {
+        console.log('clicked delete');
+        onDelete(event.target);
+    } else if (event.target.className == 'edit') {
+        console.log('clicked edit');
+    }
+}
+
+async function onDelete(button) {
+
+    const id = button.parentElement.dataset.id;
+    await deleteBook(id);
+    button.parentElement.parentElement.remove();
+}
+
+async function onCreate(event) {
+
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const author = formData.get('author');
+    const title = formData.get('title');
+
+    const result = await createBook({ author, title });
+    tbody.appendChild(createRow(result._id, result));
+    event.target.reset();
+}
 
 async function loadBooks() {
 
@@ -14,9 +48,9 @@ function createRow(id, book) {
     const row = document.createElement('tr');
     row.innerHTML = `<td>${book.title}</td>
     <td>${book.author}</td>
-    <td>
-        <button>Edit</button>
-        <button data-id=${id}>Delete</button>
+    <td data-id=${id}>
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
     </td>`;
 
     return row;
