89b6f5df735d23527f04749a768a973ea4a48a11
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-06 12:15:35 +0200

    Update app.js

--- 03. Data-and-Authentication/Exercise/02. Phonebook/app.js
+++ 03. Data-and-Authentication/Exercise/02. Phonebook/app.js
@@ -1,5 +1,30 @@
 function attachEvents() {
-    console.log('TODO...');
+
+
 }
 
 attachEvents();
+
+async function loadContacts() {
+
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook');
+    const data = await res.json();
+
+    return data;
+}
+
+async function createContact(contact) {
+
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+    });
+
+    const data = await res.json();
+
+    return data;
+
+}
