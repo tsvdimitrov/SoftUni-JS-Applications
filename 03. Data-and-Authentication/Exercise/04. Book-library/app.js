68d22a8c4f2c9b3d50335ec0efd6651b491c9ddc
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-06 20:03:05 +0200

    Update app.js

--- 03. Data-and-Authentication/Exercise/04. Book-library/app.js
+++ 03. Data-and-Authentication/Exercise/04. Book-library/app.js
@@ -1,1 +1,59 @@
-console.log('My requests...')
+
+async function request(url, options) {
+
+    if (options.body != undefined) {
+        Object.assign(options, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+    }
+
+    const response = await fetch(url, options);
+
+    if (response.ok != true) {
+        const error = await response.json();
+        alert(error.message);
+        throw new Error(error.message);
+    }
+    const data = await response.json();
+
+    return data;
+}
+
+async function loadBooks() {
+
+    const books = await request('http://localhost:3030/jsonstore/collections/books');
+
+    return books;
+}
+
+async function createBook(book) {
+
+    const result = await request('http://localhost:3030/jsonstore/collections/books', {
+        method: 'post',
+        body: JSON.stringify(book)
+    });
+
+    return result;
+}
+
+async function updateBook(id, book) {
+
+    const result = await request('http://localhost:3030/jsonstore/collections/books/' + id, {
+        method: 'put',
+        body: JSON.stringify(book)
+    });
+
+    return result;
+}
+
+async function deleteBook(id) {
+
+    const result = await request('http://localhost:3030/jsonstore/collections/books/' + id, {
+        method: 'delete',
+
+    });
+
+    return result;
+}
