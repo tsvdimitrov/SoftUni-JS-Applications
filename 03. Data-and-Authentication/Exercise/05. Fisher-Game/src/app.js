910c407a19e1d023613672b77ee6b5b663ad3934
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-07 10:06:10 +0200

    Update app.js

--- 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/app.js
+++ 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/app.js
@@ -10,3 +10,32 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.load').addEventListener('click', loadData);
 });
 
+async function loadData() {
+
+    const res = await fetch('http://localhost:3030/data/catches');
+    const data = await res.json();
+
+    document.getElementById('catches').replaceChildren(...data.map(createPreview));
+}
+
+function createPreview(item) {
+
+    const element = document.createElement('div');
+    element.className = 'catch';
+    element.innerHTML = `<label>Angler</label>
+    <input type="text" class="angler" value="${item.angler}">
+    <label>Weight</label>
+    <input type="text" class="weight" value="${item.weight}">
+    <label>Species</label>
+    <input type="text" class="species" value="${item.species}">
+    <label>Location</label>
+    <input type="text" class="location" value="${item.location}">
+    <label>Bait</label>
+    <input type="text" class="bait" value="${item.bait}">
+    <label>Capture Time</label>
+    <input type="number" class="captureTime" value="${item.captureTime}">
+    <button class="update" data-id="${item._id}">Update</button>
+    <button class="delete" data-id="${item._id}">Delete</button>`
+
+    return element;
+}
