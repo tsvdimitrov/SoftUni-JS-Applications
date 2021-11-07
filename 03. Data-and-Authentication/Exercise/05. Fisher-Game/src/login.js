5b50bc13e1be72cdcf7787e45afd9d49a62b6230
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-07 11:06:34 +0200

    Update login.js

--- 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/login.js
+++ 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/login.js
@@ -1,10 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
-    form.addEventListener('submit', onLogin);
-});
+    form.addEventListener('submit', login);
+})
 
-async function onLogin(event) {
-
+async function login(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
 
@@ -12,20 +11,17 @@ async function onLogin(event) {
     const password = formData.get('password');
 
     try {
-        const res = await fetch('http://localhost:3030/users/login', {
+        const response = await fetch('http://localhost:3030/users/login', {
             method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
         });
 
-        if (res.ok != true) {
-            const error = await res.json();
+        if (response.ok != true) {
+            const error = await response.json();
             throw new Error(error.message);
         }
-
-        const data = await res.json();
+        const data = await response.json();
         const userData = {
             email: data.email,
             id: data._id,
@@ -33,10 +29,9 @@ async function onLogin(event) {
         };
 
         sessionStorage.setItem('userData', JSON.stringify(userData));
-        window.location = '/index.html';
-
-    } catch (err) {
-
+        window.location = './index.html';
+    }
+    catch (error) {
         alert(error.message);
     }
 }
