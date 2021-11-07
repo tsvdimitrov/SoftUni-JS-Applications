1729ed505deb8d04132652727e47eaff85b38db7
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-07 09:32:56 +0200

    Update login.js

--- 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/login.js
+++ 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/login.js
@@ -2,3 +2,33 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('login');
     form.addEventListener('submit', onLogin);
 });
+
+async function onLogin(event) {
+
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+        const res = await fetch('http:localhost:3030/users/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (res.ok != true) {
+            const error = await res.json();
+            throw new Error(error.message);
+        }
+
+        const data = await res.json();
+        const token = data.accessToken;
+
+    } catch (err) {
+
+    }
+}
