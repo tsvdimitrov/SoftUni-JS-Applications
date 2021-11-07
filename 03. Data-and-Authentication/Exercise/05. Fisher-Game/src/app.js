a46f7b35e44424f4a0d0fb9d7593fc24de971230
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-07 09:52:10 +0200

    Update app.js

--- 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/app.js
+++ 03. Data-and-Authentication/Exercise/05. Fisher-Game/src/app.js
@@ -2,9 +2,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const userData = JSON.parse(sessionStorage.getItem('userData'));
     if (userData != null) {
         document.getElementById('guest').style.display = 'none';
-        document.getElementById
+        document.querySelector('#addForm .add').disabled = false;
     } else {
         document.getElementById('user').style.display = 'none';
     }
 
+    document.querySelector('.load').addEventListener('click', loadData);
 });
+
